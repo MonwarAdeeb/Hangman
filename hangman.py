bc374e90498960faaffe03d8712af0624a4f2ed9
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-02-14 11:25:39 +0600

    Worked on Hangman

--- hangman.py
+++ hangman.py
@@ -5,3 +5,11 @@ pygame.init()
 winHeight = 480
 winWidth = 700
 win = pygame.display.set_mode((winWidth, winHeight))
+
+# initialize global variables/constants #
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+LIGHT_BLUE = (102, 255, 255)
